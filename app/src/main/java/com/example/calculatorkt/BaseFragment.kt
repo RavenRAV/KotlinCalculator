2e6dbebae1b85ecedc2a646fbeefb675d373a89d
RavenRav <akilaia@inbox.ru>
2021-05-26 11:54:37 +0600

    m

--- app/src/main/java/com/example/calculatorkt/BaseFragment.kt
+++ app/src/main/java/com/example/calculatorkt/BaseFragment.kt
@@ -7,9 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
-import com.example.calculatorkt.viewModel.BaseViewModel
+import com.example.calculatorkt.base.BaseViewModel
 
-abstract class BaseFragment<Binding: ViewBinding, VM: BaseViewModel>(private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> Binding, className: Class<VM>): Fragment() {
+abstract class BaseFragment<Binding: ViewBinding, VM: BaseViewModel>
+    (private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> Binding, className: Class<VM>): Fragment() {
 
     private var _binding: Binding? = null
     val binding get() = _binding!!
